eb87c6227b259a7ac70d5db8acfb89c4281a1308
Wayne <wayinone@gmail.com>
2025-05-05 16:05:36 -0400

    better form control on sign up page

--- app/routes/sign-up.tsx
+++ app/routes/sign-up.tsx
@@ -10,6 +10,10 @@ import {
 import { API_URL, DEMO_API_URL } from '~/constants';
 import { useTranslation } from 'react-i18next';
 import { getFixedT } from '~/i18next.server';
+import { isStrongPassword } from '~/utils/ensure-strong-password';
+import { useState } from 'react';
+
+const EMAIL_REGEX = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
 export async function action({ request }: ActionFunctionArgs) {
   if (API_URL === DEMO_API_URL) {
@@ -40,9 +44,43 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export default function SignUpPage() {
+  const [emailFeedback, setEmailFeedback] = useState<string | null>('Email Address is required');
+  const [passwordFeedback, setPasswordFeedback] = useState<string[] | null>(['Password is required']);
+  const [repeatPasswordFeedback, setRepeatPasswordFeedback] = useState<string | null>('Please repeat password!');
   const [searchParams] = useSearchParams();
   const formErrors = useActionData<RegisterValidationErrors>();
   const { t } = useTranslation();
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const email = event.target.value;
+    if (!email) {
+      setEmailFeedback('Email Address is required'); 
+      return;
+    }
+    if (!EMAIL_REGEX.test(email)) {
+      setEmailFeedback('Invalid email address');
+    } else {
+      setEmailFeedback(null); // Clear feedback if the email is valid
+    }
+  }
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const password = event.target.value;
+
+    const email = (document.getElementById('email') as HTMLInputElement)?.value || '';
+    const validation = isStrongPassword(password, email);
+    setPasswordFeedback(validation.isValid ? null : validation.errorMessages || ['Invalid password']);
+  };
+
+  const handleRepeatPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const repeatPassword = event.target.value;
+    const password = (document.getElementById('password') as HTMLInputElement)?.value || '';
+    if (repeatPassword !== password) {
+      setRepeatPasswordFeedback('Passwords do not match');
+    } else {
+      setRepeatPasswordFeedback(null); // Clear feedback if the passwords match
+    }
+  };
 
   return (
     <>
@@ -85,7 +123,13 @@ export default function SignUpPage() {
                     type="email"
                     autoComplete="email"
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
+                    onChange={handleEmailChange}
                   />
+                  {emailFeedback && (
+                    <div className="text-xs text-red-700">
+                      {emailFeedback}
+                    </div>
+                  )}
                   {formErrors?.email && (
                     <div className="text-xs text-red-700">
                       {formErrors.email}
@@ -138,13 +182,27 @@ export default function SignUpPage() {
                   {t('account.password')}
                 </label>
                 <div className="mt-1">
+                  {/* emailFeedback needs to be null to allow input */}
                   <input
                     id="password"
                     name="password"
                     type="password"
                     autoComplete="current-password"
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
+                    className={`appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm ${
+                      !emailFeedback // when emailFeedback is null, allow input
+                        ? 'border-gray-300 focus:ring-primary-500 focus:border-primary-500'
+                        : 'bg-gray-100 border-gray-200 cursor-not-allowed'
+                    }`}
+                    onChange={handlePasswordChange}
+                    disabled={!!emailFeedback}
                   />
+                  {!emailFeedback && passwordFeedback && (
+                    <div className="text-xs text-red-700">
+                      {passwordFeedback.map((message, index) => (
+                        <div key={index}>{message}</div>
+                      ))}
+                    </div>
+                  )}
                   {formErrors?.password && (
                     <div className="text-xs text-red-700">
                       {formErrors.password}
@@ -165,8 +223,19 @@ export default function SignUpPage() {
                     name="repeatPassword"
                     type="password"
                     autoComplete="current-password"
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
+                    disabled={!!passwordFeedback || !!emailFeedback}
+                    onChange={handleRepeatPasswordChange}
+                    className={`appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm ${
+                      !passwordFeedback && !emailFeedback
+                        ? 'border-gray-300 focus:ring-primary-500 focus:border-primary-500'
+                        : 'bg-gray-100 border-gray-200 cursor-not-allowed'
+                    }`}
                   />
+                  {!passwordFeedback && repeatPasswordFeedback && (
+                    <div className="text-xs text-red-700">
+                      {repeatPasswordFeedback}
+                    </div>
+                  )}
                   {formErrors?.repeatPassword && (
                     <div className="text-xs text-red-700">
                       {formErrors.repeatPassword}
@@ -198,7 +267,18 @@ export default function SignUpPage() {
               <div>
                 <button
                   type="submit"
-                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+                  disabled={
+                    !!emailFeedback ||
+                    !!passwordFeedback ||
+                    !!repeatPasswordFeedback
+                  }
+                  className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white ${
+                    !emailFeedback &&
+                    !passwordFeedback &&
+                    !repeatPasswordFeedback
+                      ? 'bg-primary-600 hover:bg-primary-700 focus:ring-2 focus:ring-offset-2 focus:ring-primary-500'
+                      : 'bg-gray-300 cursor-not-allowed'
+                  }`}
                 >
                   {t('account.signUp')}
                 </button>
